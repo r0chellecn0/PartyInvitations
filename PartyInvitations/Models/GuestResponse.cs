0ab2d1c5fc0394495ca42d82a9c48594abef7b70
Rochelle Gorbea <Rochelle.gorbea@outlook.com>
2019-02-20 15:12:51 -0800

    Initial commit

---
+++ PartyInvitations/Models/GuestResponse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvitations.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter your e-mail")]
+        [RegularExpression(".+\\@.+\\..+",
+            ErrorMessage="please enter a valid email address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter your phone number")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Please let me know if you're coming or not")]
+        public bool ? WillAttend { get; set; }
+
+    }
+}
